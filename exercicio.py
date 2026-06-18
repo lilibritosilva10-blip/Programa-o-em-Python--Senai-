9dd9884dd35aadb1ff2004c3527dbcaa7e319c0e
lilibritosilva10-blip <lilibritosilva10@gmail.com>
2026-06-18 16:34:53 -0300

    Add files via upload

---
+++ exercicio.py
@@ -0,0 +1,38 @@
+print
+numero= list(range( 2,21,2))
+print(numero)
+
+
+numero=[1,2,3,4,5,6,7,8,9,10]
+print(numero)
+
+print(numero[3])
+
+numero.append(9)
+print(numero)
+
+numero.pop(5)
+print(numero)
+
+carros=['t-cross','kia sou', 'fiat toro']
+final= numero + carros
+print(final)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
